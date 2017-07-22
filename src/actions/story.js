64fcd3e7ef1b50a08b68d8bc271b8557a9aded62
Robin Wieruch <wrobin@gmx.net>
2017-07-22 20:00:57 +0200

    error handling

--- src/actions/story.js
+++ src/actions/story.js
@@ -1,6 +1,7 @@
 import {
   STORIES_ADD,
   STORIES_FETCH,
+  STORIES_FETCH_ERROR,
 } from '../constants/actionTypes';
 
 const doAddStories = stories => ({
@@ -13,7 +14,13 @@ const doFetchStories = query => ({
   query,
 });
 
+const doFetchErrorStories = error => ({
+  type: STORIES_FETCH_ERROR,
+  error,
+});
+
 export {
   doAddStories,
   doFetchStories,
+  doFetchErrorStories,
 };
--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,3 +1,4 @@
 export const STORY_ARCHIVE = 'STORY_ARCHIVE';
 export const STORIES_FETCH = 'STORIES_FETCH';
+export const STORIES_FETCH_ERROR = 'STORIES_FETCH_ERROR';
 export const STORIES_ADD = 'STORIES_ADD';
