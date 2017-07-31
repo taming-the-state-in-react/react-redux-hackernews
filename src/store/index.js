652e6419e2a872ba2d1dd65465006b13f0799c4f
Robin Wieruch <wrobin@gmx.net>
2017-07-31 18:26:28 +0200

    part 10

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers';
 
+const logger = createLogger();
+
 const store = createStore(
-  rootReducer
+  rootReducer,
+  undefined,
+  applyMiddleware(logger)
 );
 
 export default store;
