5e3338d3ffff924b7a12eccb691365fd11cb5aed
Robin Wieruch <wrobin@gmx.net>
2017-07-31 18:24:36 +0200

    part 8

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import store from './store';
+import { getReadableStories } from './selectors/story';
 import { STORY_ARCHIVE } from './constants/actionTypes';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <App
-    stories={store.getState().storyState}
+    stories={getReadableStories(store.getState())}
     onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
   />,
   document.getElementById('root')
