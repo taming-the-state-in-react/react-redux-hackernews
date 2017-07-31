a0af4224efc0b1ed2d349aaf60946a7a7dd355cb
Robin Wieruch <wrobin@gmx.net>
2017-07-31 18:00:00 +0200

    init

--- src/api/story.js
+++
@@ -1,9 +0,0 @@
-const HN_BASE_URL = 'http://hn.algolia.com/api/v1/search?query=';
-
-const fetchStories = query =>
-  fetch(HN_BASE_URL + query)
-    .then(response => response.json());
-
-export {
-  fetchStories,
-};
--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './components/App';
-import store from './store';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
