6fc1973c3fc2dfbcc08bd047af6808ee6903568a
Robin Wieruch <wrobin@gmx.net>
2017-09-11 18:53:32 +0200

    add https

--- src/api/story.js
+++ src/api/story.js
@@ -1,4 +1,4 @@
-const HN_BASE_URL = 'http://hn.algolia.com/api/v1/search?query=';
+const HN_BASE_URL = 'https://hn.algolia.com/api/v1/search?query=';
 
 const fetchStories = query =>
   fetch(HN_BASE_URL + query)
